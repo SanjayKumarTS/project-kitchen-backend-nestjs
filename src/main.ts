4e762ffc63366866dea857066822d7ed7a0e44cc
Sanjay Kumar T S <sanjay.ts1997@gmail.com>
2023-12-09 09:06:06 -0500

    made changes to main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3005);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
